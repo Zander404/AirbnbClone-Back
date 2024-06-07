748dabc6e9f9d050f35c4ea3d32372626a40411b
nox <alexandrearthurpg@gmail.com>
2024-06-07 18:51:27 -0300

    in 7h and 30min

--- property/api.py
+++ property/api.py
@@ -4,8 +4,8 @@ from rest_framework.decorators import api_view, authentication_classes, permissi
 
 
 from .models import Property
-from .serializers import PropertiesListSerializer
-
+from .forms import PropertyForm
+from .serializers import PropertiesDetailSerializer, PropertiesListSerializer
 
 
 @api_view(['GET'])
@@ -18,3 +18,31 @@ def properties_list(request):
     return JsonResponse({
         'data': serializer.data
     })
+
+
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def properties_detail(request, pk):
+    property = Property.objects.get(pk=pk)
+
+    serializer = PropertiesDetailSerializer(property, many=False)
+
+    return JsonResponse(serializer.data)
+
+
+
+@api_view(['POST', 'FILES'])
+def create_property(request):
+    form = PropertyForm(request.POST, request.FILES)
+
+    if form.is_valid():
+        property = form.save(commit=False)
+        property.landlord  = request.user
+        property.save()
+
+        return JsonResponse({'sucess': True})
+    
+    else:
+        print('error', form.errors, form.non_field_errors)
+        return JsonResponse({'errors': form.errors.as_json()}, status='400')
